f8c289a29fc112ec27eef9e466c175b2ebb85924
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2026-02-11 16:30:20 +0100

    fix

---
+++ src/types/global.d.ts
@@ -0,0 +1,11 @@
+export { };
+
+declare global {
+    interface Window {
+        _env_?: {
+            AUTH_SERVICE_URL?: string;
+            TWITCH_CLIENT_ID?: string;
+            FRONT_URL?: string;
+        };
+    }
+}
