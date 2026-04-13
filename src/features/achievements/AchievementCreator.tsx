d4bc9a85be42e67281b35aca67585627681284c2
Maxcwt <148951047+Maxcwt@users.noreply.github.com>
2026-04-13 13:43:56 +0200

    s

--- src/features/achievements/AchievementCreator.tsx
+++ src/features/achievements/AchievementCreator.tsx
@@ -178,7 +178,7 @@ export function AchievementCreator({
       } else {
         await achievementManagementClient.createAchievement({
           ...normalizedPayload,
-          channelId: selectedChannel.id,
+          channelId: selectedChannel!.id,
         })
         setSubmitSuccess(`Achievement "${normalizedPayload.title}" was published.`)
       }
